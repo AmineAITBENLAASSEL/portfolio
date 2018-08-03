52a493fde5db04651e917c24d237e91516c4f340
amine <amine.aitbenlaassel@gmail.com>
2018-08-03 12:32:39 +0100

    new portfolio

---
+++ portfolio/take/config.rb
@@ -0,0 +1,10 @@
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "css/sass"
+images_dir = "css/img"
+
+output_style = :nested
+environment = :development
+
+relative_assets = true
